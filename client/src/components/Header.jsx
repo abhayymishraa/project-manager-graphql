2e9c4beebc4ff0665eabe085b4a79a9533d581cc
AbhayMishra <grabhaymishra@gmail.com>
2024-05-26 15:15:44 +0530

    Added modification

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -6,7 +6,7 @@ export default function Header() {
             <a className='navbar-brand' href='/'>
             <div className='d-flex'>
               <img src={logo} alt='logo' className='mr-2' />
-              <div>GraphQL</div>
+              <div>Project-Manager-GraphQL</div>
             </div>
             </a>
        </div>
